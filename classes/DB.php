5568a2f2e14ae97681bf8a030c776288594e6b6a
Łukasz Otowski <lukaszotowski@interia.pl>
2018-09-19 14:53:07 +0200

    supplement commentaries

--- classes/DB.php
+++ classes/DB.php
@@ -1,12 +1,14 @@
 <?php
 class DB {
     
+    //connect with database method
     private static function connect() {
         $pdo = new PDO('mysql:host=127.0.0.1;dbname=social-network;charset=utf8', 'root', '');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
     
+    //method to interact with database
     public static function query($query, $params = array()) {
         $statement = self::connect()->prepare($query);
         $statement->execute($params);
--- create-account.php
+++ create-account.php
@@ -1,29 +1,38 @@
 <?php
 include('classes/DB.php');
 
+//run only when $_POST['createaccount'] is exist
 if (isset($_POST['createaccount'])){
+    //create variable for handling user
     $username = $_POST['username'];
     $password = $_POST['password'];
     $email = $_POST['email'];
     
+    //run only when specific username is not exist
     if(!DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$username))){
         
-        //valid length of username value
+        //validate length of username value
         if (strlen($username) >= 3 && strlen($username) <= 32) {
             
+            //validate charachters of username by php regex function
             if (preg_match('/[a-zA-Z0-9_]+/', $username)) {
                     
-                    if(strlen($password)>= 6 && strlen($password) <= 60) {
-                
-                        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                            DB::query('INSERT INTO users VALUES (\'\', :username, :password, :email)', array(':username'=>$username, ':password'=>password_hash($password, PASSWORD_BCRYPT),':email'=>$email));
-                            echo 'Success!';
-                        } else {
-                            echo 'Invalid email';
-                        }
+                //validate length of password
+                if(strlen($password)>= 6 && strlen($password) <= 60) {
+                    
+                    //validate email address
+                    if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        //insert data into database
+                        DB::query('INSERT INTO users VALUES (\'\', :username, :password, :email)', array(':username'=>$username, ':password'=> 
+                        //hash password
+                        password_hash($password, PASSWORD_BCRYPT),':email'=>$email));
+                        echo 'Success!';
                     } else {
-                        echo 'Invalid password';
+                        echo 'Invalid email';
                     }
+                } else {
+                    echo 'Invalid password';
+                }
             } else {
                 echo 'Invalid username';
             }
@@ -38,9 +47,11 @@ if (isset($_POST['createaccount'])){
 
 
 <h1>Register</h1>
+<!--LOGIN FORM START-->
 <form action="create-account.php" method="post">
     <input type="text" name="username" value="" placeholder="Username..."> </p>
     <input type="password" name="password" value="" placeholder="Password..."> </p>
     <input type="email" name="email" value="" placeholder="someone@somesite.com"> </p>
     <input type="submit" name="createaccount" value="Create Account"> </p>
 </form>
+<!--LOGIN FORM END-->
